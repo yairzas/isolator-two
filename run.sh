5a50f69e7fda424ffc2a9363ccca9bcdee72fbe0
Tolik Litovsky <anatoly.lit@gmail.com>
2016-03-16 21:31:40 +0200

    quacd is running on the port
    Signed-off-by: Tolik Litovsky <anatoly.lit@gmail.com>

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 Xvfb -screen 0 1024x768x16 -ac &
 export DISPLAY=:0
 x11vnc -rfbauth /root/.vnc/passwd -display :0 -forever &
-/usr/bin/firefox &
-cd /root/noVNC;./utils/launch.sh --vnc 0.0.0.0:5900
+/usr/local/sbin/guacd -b 0.0.0.0 -f &
+/usr/bin/firefox
