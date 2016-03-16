19658a2f7d926a41a04b8221ccc351f7d4e6392d
Tolik Litovsky <anatoly.lit@gmail.com>
2016-03-16 22:16:56 +0200

    Adding download guacd sh that can deploy its war to tomcat
    Signed-off-by: Tolik Litovsky <anatoly.lit@gmail.com>

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ Xvfb -screen 0 1024x768x16 -ac &
 export DISPLAY=:0
 x11vnc -rfbauth /root/.vnc/passwd -display :0 -forever &
 /usr/local/sbin/guacd -b 0.0.0.0 -f &
+/usr/sbin/tomcat start &
 /usr/bin/firefox
